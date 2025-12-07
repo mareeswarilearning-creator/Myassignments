f0160219f6a68a5634f627e934a74370e0be6ed1
mareeswarilearning-creator <mareeswarilearning@gmail.com>
2025-12-07 08:47:46 +0530

    Add files via upload

---
+++ Assignment1.js
@@ -0,0 +1,11 @@
+let firstName="testleaf"
+let companyName="test"
+let mobileNumber=9894637889
+let isAutomation=true
+let hasPlaywright
+
+console.log("First Name "+firstName+"\n"+"Type is "+typeof firstName)
+console.log("Company Name "+companyName+"\n"+"Type is "+typeof companyName)
+console.log("Mobile number "+mobileNumber+"\n"+"Type is "+typeof mobileNumber)
+console.log("Is Automation ? "+isAutomation+"\n"+"Type is "+typeof isAutomation)
+console.log("hasPlaywright "+hasPlaywright+"\n"+"Type is "+typeof hasPlaywright)
