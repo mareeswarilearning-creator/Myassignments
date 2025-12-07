94447ba6abf2b2f909afc3adbfbb814bf0a3037b
mareeswarilearning-creator <mareeswarilearning@gmail.com>
2025-12-07 11:03:43 +0530

    Add files via upload

---
+++ Conditional_Loop.js
@@ -0,0 +1,17 @@
+let mark=55
+if(mark>85){
+    console.log("Grade A")
+}else if(mark>60){
+    console.log("Grade B")
+}else if(mark>50){
+    console.log("Grade C")
+}else{
+    console.log("Grade D")
+}
+
+let input=30
+for(let i=0;i<=input;i++){
+    if(i%2!=0){
+        console.log("odd number"+i)
+    }
+}
